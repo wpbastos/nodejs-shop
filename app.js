761bf7ba83b19ebc984e89574462fdf09de324c7
wpbastos <wpbastos@live.com>
2020-06-12 13:01:14 -0400

    Adjust fake user

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  User.findById('5ee383c2baebc20006a92d36')
+  User.findById(process.env.USER_DEFAULT)
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
